34a22108e48f5f734562ec69d00f9913fd637c7d
axellongatte5-hue <axellongatte5@gmail.com>
2026-03-16 09:52:31 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,24 +2,11 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
-  },
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.jsx?$/,
-    exclude: [],
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
     },
   },
   server: {
