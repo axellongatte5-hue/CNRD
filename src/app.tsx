f6cb52cd49675db238f30122283536cc98b972fa
axellongatte5-hue <axellongatte5@gmail.com>
2026-03-16 09:46:51 +0100

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Nieto from './pages/Nieto';
-import Bouteille from './pages/Bouteille';
-import Feigelson from './pages/Feigelson';
+"import \"@/index.css\";
+import { BrowserRouter, Routes, Route } from \"react-router-dom\";
+import Layout from \"@/components/Layout\";
+import HomePage from \"@/pages/HomePage\";
+import PersonnagesPage from \"@/pages/PersonnagesPage\";
+import PersonnageDetail from \"@/pages/PersonnageDetail\";
+import CampsPage from \"@/pages/CampsPage\";
+import CampDetail from \"@/pages/CampDetail\";
+import CartePage from \"@/pages/CartePage\";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/nieto" element={<Nieto />} />
-        <Route path="/bouteille" element={<Bouteille />} />
-        <Route path="/feigelson" element={<Feigelson />} />
-      </Routes>
-    </Router>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path=\"/\" element={<HomePage />} />
+          <Route path=\"/personnages\" element={<PersonnagesPage />} />
+          <Route path=\"/personnage/:id\" element={<PersonnageDetail />} />
+          <Route path=\"/camps\" element={<CampsPage />} />
+          <Route path=\"/camp/:id\" element={<CampDetail />} />
+          <Route path=\"/carte\" element={<CartePage />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
 export default App;
+"
