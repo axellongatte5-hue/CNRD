db77c56b0b6fe939dc11e7766d2057d6d850006e
axellongatte5-hue <axellongatte5@gmail.com>
2026-03-20 21:57:37 +0100

    Update Nieto.tsx

--- src/pages/Nieto.tsx
+++ src/pages/Nieto.tsx
@@ -48,7 +48,7 @@ function Nieto() {
         </div>
       </div>
 
-      div id=\"chronologie\" className=\"max-w-5xl mx-auto px-8 py-16\">
+      div id=\"chronologie\" div id=\"chronologie\" className=\"max-w-5xl mx-auto px-8 py-16\">
         <h2 className="text-3xl font-bold text-center mb-16 text-gray-800">Chronologie</h2>
 
         <div className="relative">
